888d3e00f545a2adc034b9c50ec39a7d36bc2d5c
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-01 21:57:26 +0530

    withput zod, jwt

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
 import { UserSchema } from "../validators/userValidator";
-import { fromZodError } from "zod-validation-error";
-import bcrypt from "bcryptjs";
+// import { fromZodError } from "zod-validation-error";
+// import bcrypt from "bcryptjs";
 
 const getAllUser = async (req: Request, res: Response) => {
   try {
@@ -15,71 +15,41 @@ const getAllUser = async (req: Request, res: Response) => {
 };
 //
 
+// Controller function to register a new user
 const registerUser = async (req: Request, res: Response) => {
   try {
-    //
-    console.log(req.body, "req.body of registerUser");
-    //
     const { username, password } = req.body;
-    //
 
-    //
-    const validatedTask = UserSchema.safeParse({
-      username: username,
-      password: password,
+    // Create a new user
+    const newUser = new User({
+      username,
+      password,
     });
-    //
-    if (!validatedTask.success) {
-      //zod messa in a string showing
-      return res
-        .status(400)
-        .json({ error: fromZodError(validatedTask.error).message });
-    }
-    //
-    const validatedData = validatedTask.data;
-    // Generate a salt and hash the user's password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(validatedData.password, salt);
-    //
-    const user = new User({
-      username: validatedData.username,
-      password: hashedPassword,
-    });
-    await user.save();
 
-    res.json({ user: user, message: "user created" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    // Save the user to the database
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json(error); // Handle any validation or database errors
   }
 };
-
-//
+// Controller function to log in an existing user
 const loginUser = async (req: Request, res: Response) => {
-  //
-  console.log(req.body, "req.body of loginUser");
-  //
   try {
     const { username, password } = req.body;
-    // need to be findOne(find giving error )
-    const user = await User.findOne({ username: username });
 
-    if (!user) {
-      return res.status(404).json({ messsage: "user not found" });
+    // Find the user by username
+    const user = await User.findOne({ username });
+
+    // Check if the user exists and compare passwords
+    if (user && user.password === password) {
+      res.status(200).json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ error: "Invalid username or password" });
     }
-
-    console.log(user.password, "user");
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(401).json({ message: "Incorrect password" });
-    }
-
-    res.status(200).json({ user: user, message: "login successful" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(400).json(error); // Handle any errors
   }
 };
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true, unique: true },
-    password: {
-      type: String,
-      trim: true,
-      minlength: 4,
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    trim: true,
+    minlength: 4,
+    required: true,
   },
-  { timestamps: true }
-);
+});
 
 const User = mongoose.model("user", userSchema);
 
