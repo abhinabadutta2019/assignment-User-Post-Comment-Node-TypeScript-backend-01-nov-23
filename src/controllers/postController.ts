4fc734b11d514876e992dc46ed03cb2bc192dc6d
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-03 20:46:56 +0530

    addComment controller added

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -36,5 +36,42 @@ const createPost = async (req: CustomRequest, res: Response) => {
     res.status(400).json(error); // Handle any validation or database errors
   }
 };
+//
+const addComment = async (req: CustomRequest, res: Response) => {
+  try {
+    //
+    const postId = req.params.postId;
 
-export { createPost };
+    const middlewareUser = req.user;
+    console.log(middlewareUser, "middlewareUser");
+
+    const { comment } = req.body;
+    //
+    // Find the post by its ID
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    //
+    // Create a new comment object
+    const newComment = {
+      user: middlewareUser,
+      text: comment,
+    };
+    //
+    post.comments.push(newComment);
+    // Save the updated post
+    await post.save();
+
+    //
+    return res
+      .status(201)
+      .json({ message: "Comment added successfully", post });
+
+    //
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export { createPost, addComment };
--- src/routers/post.ts
+++ src/routers/post.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 const router = Router();
-import { createPost } from "../controllers/postController";
+import { createPost, addComment } from "../controllers/postController";
 import { verifyJWT } from "../middleware/verifyJWT";
 
 // Define a route for creating a new post
 router.post("/", verifyJWT, createPost);
+//
+router.post("/comment/:postId", verifyJWT, addComment);
 
 export { router as postRouter };
